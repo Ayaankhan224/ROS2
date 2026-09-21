929c3f38d7c73375ed4bf02f7283428ab5d24004
Ayaankhan224 <ayaankhan224271@gmail.com>
2026-09-21 18:59:23 +0530

    created a publisher

--- build/mini_rover/build/lib/mini_rover/robot_monitor.py
+++ build/mini_rover/build/lib/mini_rover/robot_monitor.py
@@ -1,17 +1,22 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 class RobotMonitor(Node):
 
   def __init__(self):
     super().__init__('robot_monitor')
     self.get_logger().info('Robot Monitor Started')
-    self.declare_parameter('timer_period', 2.0)
-    timer_period = self.get_parameter('timer_period').value
-    self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    self.create_timer(1.0, self.timer_callback)
+
+    self.publisher = self.create_publisher(String, 'robot_status', 10)
 
   def timer_callback(self):
-    self.get_logger().info('Robot is running....')
+    self.get_logger().info('Robot alive...')
+    msg = String()
+    msg.data = 'Robot saying hello....'
+    self.publisher.publish(msg)
 
 def main(args=None):
   rclpy.init()
