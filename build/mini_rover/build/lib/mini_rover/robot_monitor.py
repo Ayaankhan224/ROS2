7489214fd490b90bdc8d8f5488087070f24b54af
Ayaankhan224 <ayaankhan224271@gmail.com>
2026-09-21 18:34:53 +0530

    created timer in monitor node

--- build/mini_rover/build/lib/mini_rover/robot_monitor.py
+++ build/mini_rover/build/lib/mini_rover/robot_monitor.py
@@ -6,6 +6,12 @@ class RobotMonitor(Node):
   def __init__(self):
     super().__init__('robot_monitor')
     self.get_logger().info('Robot Monitor Started')
+    self.declare_parameter('timer_period', 2.0)
+    timer_period = self.get_parameter('timer_period').value
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+
+  def timer_callback(self):
+    self.get_logger().info('Robot is running....')
 
 def main(args=None):
   rclpy.init()
